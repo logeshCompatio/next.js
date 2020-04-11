51e07be5dfa18eaf4ae1d5a2bf993b28edc5b38a
mitsuki <mit123suki@gmail.com>
2020-04-12 03:33:08 +0530

    ant4+ with next9.3+ builtin-css example

---
+++ examples/with-ant-design/postcss.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  plugins: [
+    ...(process.env.NODE_ENV === 'production'
+      ? [
+          [
+            '@fullhuman/postcss-purgecss',
+            {
+              content: [
+                './pages/**/*.{js,jsx,ts,tsx}',
+                './components/**/*.{js,jsx,ts,tsx}',
+              ],
+              defaultExtractor: content =>
+                content.match(/[\w-/:]+(?<!:)/g) || [],
+              whitelistPatterns: [/ant/],
+            },
+          ],
+        ]
+      : []),
+    'postcss-preset-env',
+  ],
+}
